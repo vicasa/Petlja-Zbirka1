1ed4be4c3799ec3e4b5e8bbdd6ab4d8dc4ad394b
vicasa <44348173+vicasa@users.noreply.github.com>
2022-02-27 12:02:58 +0100

    Create 7 Део правоугаоника у првом квадранту.c

---
+++ 2 Гранање/2 Угнежђено гранање/5 Минимум и максимум два броја/7 Део правоугаоника у првом квадранту.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*duzPozDeo(a, b) je duzina dela intervala izmedju a i b koji cine pozitivni brojevi
+ne mora da bude a <= b
+*/
+int duzPozDeoInt(int a, int b){ 
+  int l, r;
+  if(a <= b) {l=a; r=b;}
+  else {l=b; r=a;}
+
+  if(l > 0) return r-l;
+  if(r > 0) return r;
+  return 0;
+}
+
+int main()
+{
+    int x1, y1, x2, y2;
+    scanf("%i %i %i %i", &x1, &y1, &x2, &y2);
+    printf("%i\n", duzPozDeoInt(x1, x2)*duzPozDeoInt(y1, y2));
+    return 0;
+}
