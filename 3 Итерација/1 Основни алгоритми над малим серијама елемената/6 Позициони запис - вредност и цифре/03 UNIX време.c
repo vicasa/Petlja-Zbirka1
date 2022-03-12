d45089662ba03e012bcfab9de7f66ba9633beb6c
vicasa <vicasava@gmail.com>
2022-03-12 22:25:51 +0100

    modify

--- 3 Итерација/1 Основни алгоритми над малим серијама елемената/6 Позициони запис - вредност и цифре/03 UNIX време.c
+++ 3 Итерација/1 Основни алгоритми над малим серијама елемената/6 Позициони запис - вредност и цифре/03 UNIX време.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-/*kopija koda  iz 1 Аритметика/2 Целобројно дељење/2 Позициони запис*/
+/*bolja varijanta (manje racuna) od koda iz 1 Аритметика/2 Целобројно дељење/2 Позициони запис*/
 int main() {
   unsigned dan, sat, minut, sekund, mili, trajanje;
   scanf("%u %u %u %u %u %u", &dan, &sat, &minut, &sekund, &mili, &trajanje);
-  mili = ((((dan*24)+sat)*60 + minut)*60 + sekund)*1000 + mili + trajanje; /*milisekundi od paljenja kompa do kraja pesme*/
-  sekund = mili/1000;
-  minut = sekund/60;
-  sat = minut/60;
-  dan = sat/24;
+  mili += trajanje;
+  sekund += mili/1000;
+  minut += sekund/60;
+  sat += minut/60;
+  dan += sat/24;
   printf("%u:%u:%u:%u:%u\n", dan, sat%24, minut%60, sekund%60, mili%1000);
   return 0;
 }
