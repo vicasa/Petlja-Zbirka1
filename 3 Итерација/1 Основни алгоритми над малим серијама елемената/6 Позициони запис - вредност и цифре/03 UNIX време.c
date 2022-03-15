0de7dacc9295fb660d1c30a325b6a5a324b7a3af
vicasa <44348173+vicasa@users.noreply.github.com>
2022-03-15 16:46:05 +0100

    Update 03 UNIX време.c
    iterativna varijanta

--- 3 Итерација/1 Основни алгоритми над малим серијама елемената/6 Позициони запис - вредност и цифре/03 UNIX време.c
+++ 3 Итерација/1 Основни алгоритми над малим серијама елемената/6 Позициони запис - вредност и цифре/03 UNIX време.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
-/*bolja varijanta (manje racuna) od koda iz 1 Аритметика/2 Целобројно дељење/2 Позициони запис*/
+
+
 int main() {
-  unsigned dan, sat, minut, sekund, mili, trajanje;
-  scanf("%u %u %u %u %u %u", &dan, &sat, &minut, &sekund, &mili, &trajanje);
-  mili += trajanje;
-  sekund += mili/1000;
-  minut += sekund/60;
-  sat += minut/60;
-  dan += sat/24;
-  printf("%u:%u:%u:%u:%u\n", dan, sat%24, minut%60, sekund%60, mili%1000);
+  unsigned tmp, ukupno=0, k;
+
+  scanf("%u", &tmp); /*ucitavamo broj dana*/
+  ukupno += tmp; /*ukupno dana*/
+
+  scanf("%u", &tmp); /*ucitavamo broj sati*/
+  ukupno *= 24; /*pretvaramo dane u sate*/
+  ukupno += tmp; /*ukupno sati*/
+
+  scanf("%u", &tmp); /*ucitavamo broj minuta*/
+  ukupno *= 60; /*pretvaramo sate u minute*/
+  ukupno += tmp; /*ukupno minuta*/
+
+  scanf("%u", &tmp); /*ucitavamo broj sekundi*/
+  ukupno *= 60; /*pretvaramo minute u sekunde*/
+  ukupno += tmp; /*ukupno sekundi*/
+
+
+  scanf("%u", &tmp); /*ucitavamo broj milisekundi*/
+  ukupno *= 1000; /*pretvaramo sekunde u milisekunde*/
+  ukupno += tmp; /*ukupno milisekundi*/
+
+  scanf("%u", &tmp); /*ucitavamo trajanje pesme u milisekundama*/
+  ukupno += tmp; /*ukupno milisekundi*/
+
+  unsigned s = ukupno/1000, m = s/60, h = m/60;
+  printf("%u:%u:%u:%u:%u\n", h/24, h%24, m%60, s%60, ukupno%1000);
   return 0;
 }
