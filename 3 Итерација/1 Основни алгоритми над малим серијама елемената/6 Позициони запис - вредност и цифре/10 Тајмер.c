8d9b17567a63b30711a68c419bef6c4c3d80fdf4
vicasa <vicasava@gmail.com>
2022-03-15 23:46:06 +0100

    update

--- 3 Итерација/1 Основни алгоритми над малим серијама елемената/6 Позициони запис - вредност и цифре/10 Тајмер.c
+++ 3 Итерација/1 Основни алгоритми над малим серијама елемената/6 Позициони запис - вредност и цифре/10 Тајмер.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 
+
 int main() {
-  unsigned h, m, s, t; 
-  scanf("%u %u %u", &h, &m, &s);
+  unsigned tmp, ukupno=0;
   
-  scanf("%u", &t); /*ucitavamo sate sa tajmera i dodajemo ih na sate sa casovnika*/
-  h += t;
+  scanf("%u", &tmp); /*ucitavamo sate*/
+  ukupno += tmp*3600; 
+
+  scanf("%u", &tmp); /*ucitavamo minute*/
+  ukupno += tmp*60; 
+
+  scanf("%u", &tmp); /*ucitavamo sekunde*/
+  ukupno += tmp; 
+
+  scanf("%u", &tmp); /*ucitavamo broj sati sa tajmeru*/
+  ukupno += tmp*3600; 
+
+  scanf("%u", &tmp); /*ucitavamo broj minuta sa tajmera*/
+  ukupno += tmp*60; 
+
+  scanf("%u", &tmp); /*ucitavamo broj sekundi sa tajmera*/
+  ukupno += tmp; 
   
-  scanf("%u", &t); /*ucitavamo minute sa tajmera i dodajemo ih na minute sa casovniku*/
-  m += t;
-	   
-  scanf("%u", &t);/*ucitavamo sekunde sa tajmera i dodajemo ih na sekunde sa casovniku*/
-  s += t;
+  tmp = ukupno/60; /*ukupno celih minuta od pocetka aktuelnog dana do zvona tajmera*/
   
-  m += s/60; /*prebacili paketice od po 60 sekundi u minute (1 paketic od 60 skundi = 1 minut)*/
-  h += m/60; /*prebacili paketice od po 60 minuta u sate (1 paketic od 60 minuta = 1 sat)*/
+  unsigned h = tmp/60; /*ukupno celih sati od --||--*/
   
-  /*nakon ovoga h sati + m%60 minuta + s%60 predstavlja vremenski interval od pocetka aktuelnog dana do zvona tajmera*/
-  
-  printf("%u:%u:%u+%u\n", h%24, m%60, s%60, h/24);
+  printf("%u:%u:%u+%u\n", h%24, tmp%60, ukupno%60, h/24);
   return 0;
 }
